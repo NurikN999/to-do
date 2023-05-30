796357fb46559f2870eabcf3f66fe38b067975dd
nurmukhamed <nurkamalnurmukhamed@gmail.com>
2023-05-30 22:58:19 +0600

    feat: register page done

--- app.py
+++ app.py
@@ -7,24 +7,47 @@ app.secret_key = '!)@.Y~VqN0+F[;O'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(200))
-    tasks = db.relationship('Task', lazy=True)
+    tasks = db.relationship('Tasks', lazy=True)
+
+    def __init__(self, username, email,  password):
+        self.username = username
+        self.email = email
+        self.password = password
 
 
-class Task(db.Model):
+class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     status = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
 @app.route('/', methods=['GET'])
 def main_page():
     return render_template('main.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('auth/register.html')
+    else:
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirmPassword')
+
+        if password == confirm_password:
+            user = Users(username=username, email=email, password=password)
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect('/login')
 
 
 if __name__ == '__main__':
